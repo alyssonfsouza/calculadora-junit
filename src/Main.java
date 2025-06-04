64b43c42807d90ee3f10121c3a10d5da3edc1b82
unknown <alyssons2005.14@gmail.com>
2025-06-03 22:24:15 -0300

    Adicionado o menu de interação.

--- src/Main.java
+++ src/Main.java
@@ -5,30 +5,42 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         Calculadora calc = new Calculadora();
 
-        System.out.println("Digite o primeiro número:");
-        int a = scanner.nextInt();
 
-        System.out.println("Digite o segundo número:");
-        int b = scanner.nextInt();
+        System.out.println("Você deseja usar a calculadora?");
+        System.out.println("--------- Sim ou Não ---------");
+        String calculo = scanner.next();
 
-        System.out.println("Escolha a operação (+, -, *, /):");
-        String op = scanner.next();
+        switch (calculo.toLowerCase()) {
 
-        int resultado = 0;
+            case "sim":
+                System.out.println("Digite o primeiro número:");
+                int a = scanner.nextInt();
 
-        try {
-            switch (op) {
-                case "+" -> resultado = calc.somar(a, b);
-                case "-" -> resultado = calc.subtrair(a, b);
-                case "*" -> resultado = calc.multiplicar(a, b);
-                case "/" -> resultado = calc.dividir(a, b);
-                default -> System.out.println("Operação inválida");
-            }
-            System.out.println("Resultado: " + resultado);
-        } catch (Exception e) {
-            System.out.println("Erro: " + e.getMessage());
+                System.out.println("Digite o segundo número:");
+                int b = scanner.nextInt();
+
+                System.out.println("Escolha a operação (+, -, *, /):");
+                String op = scanner.next();
+
+                int resultado = 0;
+
+                try {
+                    switch (op) {
+                        case "+" -> resultado = calc.somar(a, b);
+                        case "-" -> resultado = calc.subtrair(a, b);
+                        case "*" -> resultado = calc.multiplicar(a, b);
+                        case "/" -> resultado = calc.dividir(a, b);
+                        default -> System.out.println("Operação inválida");
+                    }
+                    System.out.println("Resultado: " + resultado);
+                } catch (Exception e) {
+                    System.out.println("Erro: " + e.getMessage());
+                }
+                scanner.close();
+                break;
+
+            default:
+                System.out.println("Saindo!");
         }
-
-        scanner.close();
     }
 }
